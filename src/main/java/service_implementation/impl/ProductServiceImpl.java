13b29f3e14aaf540a3705aecb85fd45be43974d8
Vitalii Sumka <vovk2001.ua@gmail.com>
2021-09-06 13:08:54 +0200

    fix error

--- src/main/java/service_implementation/impl/ProductServiceImpl.java
+++ src/main/java/service_implementation/impl/ProductServiceImpl.java
@@ -1,4 +1,33 @@
 package service_implementation.impl;
 
-public class ProductServiceImpl {
+import domain.Product;
+import shared.AbstractCRUD;
+
+import java.util.List;
+
+public class ProductServiceImpl implements AbstractCRUD<Product> {
+    @Override
+    public Product create(Product product) {
+        return null;
+    }
+
+    @Override
+    public Product read(Integer id) {
+        return null;
+    }
+
+    @Override
+    public Product update(Product product) {
+        return null;
+    }
+
+    @Override
+    public void delete(Integer id) {
+
+    }
+
+    @Override
+    public List<Product> readAll() {
+        return null;
+    }
 }
--- src/main/java/service_implementation/impl/UserServiceImpl.java
+++ src/main/java/service_implementation/impl/UserServiceImpl.java
@@ -1,4 +1,33 @@
 package service_implementation.impl;
 
-public class UserServiceImpl {
+import domain.User_domain;
+import shared.AbstractCRUD;
+
+import java.util.List;
+
+public class UserServiceImpl implements AbstractCRUD<User_domain> {
+    @Override
+    public User_domain create(User_domain user_domain) {
+        return null;
+    }
+
+    @Override
+    public User_domain read(Integer id) {
+        return null;
+    }
+
+    @Override
+    public User_domain update(User_domain user_domain) {
+        return null;
+    }
+
+    @Override
+    public void delete(Integer id) {
+
+    }
+
+    @Override
+    public List<User_domain> readAll() {
+        return null;
+    }
 }
